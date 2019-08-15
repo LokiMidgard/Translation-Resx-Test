e709e14abf37a2473e2ce39e04912d685a5492f1
Gromer <c.gromer@seeburger.de>
2019-08-15 10:01:52 +0200

    added .net core project

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            this.listBox1.Items.AddRange(System.Globalization.CultureInfo.GetCultures(System.Globalization.CultureTypes.AllCultures));
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedItem is CultureInfo culture)
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+                System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+
+                var resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+
+                foreach (Control ctl in this.Controls)
+                {
+                    resources.ApplyResources(ctl, ctl.Name);
+                }
+
+            }
+        }
+
+    }
+}
